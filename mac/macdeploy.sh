6d066582693af23cc1e547aab0bf49e5008e7967
srazi <srazi61@yahoo.com>
2017-03-06 01:37:00 +0330

    macOS: Support to run macdeploy.sh silently

--- mac/macdeploy.sh
+++ mac/macdeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Usage: ./macdeploy.sh [<full-path-to-macdeployqt>]
+# Usage: ./macdeploy.sh [<full-path-to-macdeployqt>] [<QT|NOQT>]
 #
 # macdeployqt is usually located in QTDIR/bin/macdeployqt
 # If path to macdeployqt is not specified, using it from PATH
@@ -9,9 +9,15 @@ MACDEPLOYQT="macdeployqt"
 LIBRARY_NAME="libQupZilla.2.dylib"
 PLUGINS="QupZilla.app/Contents/Resources/plugins"
 QTPLUGINS="QupZilla.app/Contents/PlugIns"
+REDISTRIBUTE="ASK"
+REDISTRIBUTE_QUESTION="Do you wish to redistribute known, missing, Qt Library plugins (y/n)? "
 
 if [ -n "$1" ]; then
  MACDEPLOYQT=$1
+fi
+
+if [ -n "$2" ]; then
+ REDISTRIBUTE=$2
 fi
 
 # cd to directory with bundle
@@ -34,12 +40,22 @@ do
  install_name_tool -change $LIBRARY_NAME @executable_path/$LIBRARY_NAME $plugin
 done
 
-# prompt and optionally copy additional Qt native plugin(s) into bundle
-echo -n "Do you wish to redistribute known, missing, Qt Library plugins (y/n)? "
-old_stty_cfg=$(stty -g)
-stty raw -echo
-answer=$( while ! head -c 1 | grep -i '[yn]'; do true; done )
-stty $old_stty_cfg
+if [[ "$REDISTRIBUTE" == "QT" ]]; then
+  echo "$REDISTRIBUTE_QUESTION Yes"
+  answer="y"
+else
+   if [[ "$REDISTRIBUTE" == "NOQT" ]]; then
+     echo "$REDISTRIBUTE_QUESTION No"
+     answer="n"
+   else
+     # prompt and optionally copy additional Qt native plugin(s) into bundle
+     echo -n $REDISTRIBUTE_QUESTION
+     old_stty_cfg=$(stty -g)
+     stty raw -echo
+     answer=$( while ! head -c 1 | grep -i '[yn]'; do true; done )
+     stty $old_stty_cfg
+   fi
+fi
 if echo "$answer" | grep -iq "^y"; then
   if [ -z ${QTDIR+x} ]; then
     printf '\nPlease set the environment variable for the Qt platform folder.\n\texample:\n\t$ export QTDIR="$HOME/Qt/5.8/clang_64"\n'
